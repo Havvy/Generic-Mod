96eaf92672ed340a1bcc6afcf070f2b033347fbe
Ezekiel <ezekielelin@me.com>
2013-07-03 10:41:34 -0400

    Removed preloadTexture for 1.6

--- tutorial/generic/client/ClientProxy.java
+++ tutorial/generic/client/ClientProxy.java
@@ -5,7 +5,7 @@ import tutorial.generic.CommonProxy;
 
 public class ClientProxy extends CommonProxy {
 	public void registerRenderers () {
-		MinecraftForgeClient.preloadTexture(ITEMS_PNG);
-		MinecraftForgeClient.preloadTexture(BLOCK_PNG);
+		//MinecraftForgeClient.preloadTexture(ITEMS_PNG);
+		//MinecraftForgeClient.preloadTexture(BLOCK_PNG);
 	}
 }
